72eba616ac5c32bd0aa3289dea995ffa5fca53c4
rjbazan <rodrigo.bazan@globant.com>
2016-08-16 23:07:45 -0300

    new observable

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from './../services/spotify.service';
-import { Artist } from '../../models/artist.model'
+import { Artist } from '../../models/artist.model';
+import { FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   moduleId: module.id,
   selector: 'search',
-  providers: [SpotifyService],
+  providers: [SpotifyService, FormBuilder],
+  directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
   templateUrl: 'search.component.html'
 })
 export class SearchComponent implements OnInit {
-  constructor(private spotifyService: SpotifyService) { }
+  search: string;
+  searchForm: FormGroup;
+  results: Artist[];
+  results$: Observable<any>;
+
+  constructor(
+    private spotifyService: SpotifyService, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      'search': ['', Validators.required]
+    });
+
+    this.results$ = this.searchForm.controls['search'].valueChanges
+      .switchMap(query => this.spotifyService.getAll(query))
+      .map(res => console.log(res));
+  }
 
   ngOnInit() { }
-  search: string;
-  results: Artist[];
-  searchNow() {
+ 
+  /*searchNow() {
     this.spotifyService.getAll(this.search)
       .subscribe(res => {
         this.results = res.artists.items;
       });
-  }
+  }*/
 
 }
